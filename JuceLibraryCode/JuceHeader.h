3099645e3e307c07a2242d62f03192f992eae553
Ben Kuper <bkuperberg@gmail.com>
2017-05-10 13:10:17 +0200

    Linux Juce 5

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -10,8 +10,7 @@
 
 */
 
-#ifndef __APPHEADERFILE_QZW0VV__
-#define __APPHEADERFILE_QZW0VV__
+#pragma once
 
 #include "AppConfig.h"
 
@@ -47,5 +46,3 @@ namespace ProjectInfo
     const int          versionNumber  = 0x10000;
 }
 #endif
-
-#endif   // __APPHEADERFILE_QZW0VV__
