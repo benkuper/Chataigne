712537a75d9d05e03e3b51d38aa4165e4843c1ee
Ben Kuper <bkuperberg@gmail.com>
2018-02-23 14:20:30 +0100

    Jucer update 1.3.0b

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
-    const char* const  versionString  = "1.3.0";
+    const char* const  versionString  = "1.3.0b";
     const int          versionNumber  = 0x10300;
 }
 #endif
