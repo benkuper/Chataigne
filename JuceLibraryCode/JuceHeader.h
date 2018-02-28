f2f119fdf7192df276bcc4a049fbc1bfed81a3d4
Ben Kuper <bkuperberg@gmail.com>
2018-02-28 01:25:05 +0100

    1.3.2 fixes colors when editing

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
-    const char* const  versionString  = "1.3.1";
-    const int          versionNumber  = 0x10301;
+    const char* const  versionString  = "1.3.2";
+    const int          versionNumber  = 0x10302;
 }
 #endif
