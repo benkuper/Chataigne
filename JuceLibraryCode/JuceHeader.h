908ee326e4d2fa8cc83eeab1294f27b708d04a63
Ben Kuper <bkuperberg@gmail.com>
2018-04-01 17:25:06 -0700

    Chataigne 1.3.4 version WIP

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
-    const char* const  versionString  = "1.3.3";
-    const int          versionNumber  = 0x10303;
+    const char* const  versionString  = "1.3.4";
+    const int          versionNumber  = 0x10304;
 }
 #endif
