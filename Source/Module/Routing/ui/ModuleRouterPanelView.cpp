c2f24dc04d44ef151f411f58dcb0900d96781f96
Ben Kuper <bkuperberg@gmail.com>
2018-04-02 18:38:20 -0700

    Router view larger ui

--- Source/Module/Routing/ui/ModuleRouterPanelView.cpp
+++ Source/Module/Routing/ui/ModuleRouterPanelView.cpp
@@ -34,7 +34,7 @@ ModuleRouterPanelView::~ModuleRouterPanelView()
 void ModuleRouterPanelView::resized()
 {
 	Rectangle<int> r = getLocalBounds();
-	mui.setBounds(r.removeFromLeft(150));
+	mui.setBounds(r.removeFromLeft(250));
 	r.removeFromLeft(2);
 	view.setBounds(r);
 }
