cc480fe0446c4218a8b0dcfc1dd9824f13c62f9f
Ben Kuper <bkuperberg@gmail.com>
2018-05-25 18:28:01 +0200

    1.4.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
-    const char* const  versionString  = "1.4.0b";
+    const char* const  versionString  = "1.4.0";
     const int          versionNumber  = 0x10400;
 }
 #endif
