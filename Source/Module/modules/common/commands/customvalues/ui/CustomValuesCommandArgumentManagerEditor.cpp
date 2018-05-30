5dadf4317face263c06efeee5bc1d24bbba035a6
Ben Kuper <bkuperberg@gmail.com>
2018-05-30 11:44:05 +0200

    fix popupmenu index offset

--- Source/Module/modules/common/commands/customvalues/ui/CustomValuesCommandArgumentManagerEditor.cpp
+++ Source/Module/modules/common/commands/customvalues/ui/CustomValuesCommandArgumentManagerEditor.cpp
@@ -25,9 +25,9 @@ void CustomValuesCommandArgumentManagerEditor::showMenuAndAddItem(bool)
 	p.addItem(2, "Add Float Argument");
 	p.addItem(3, "Add String Argument");
 	p.addItem(4, "Add Boolean Argument");
-	p.addItem(4, "Add Color Argument");
-	p.addItem(5, "Add Point2D Argument");
-	p.addItem(6, "Add Point3D Argument");
+	p.addItem(5, "Add Color Argument");
+	p.addItem(6, "Add Point2D Argument");
+	p.addItem(7, "Add Point3D Argument");
 
 	int result = p.show();
 
