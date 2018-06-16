1ecd477bc7aaf337c00b0f3c2ad670c8c16e11cb
Ben Kuper <bkuperberg@gmail.com>
2018-06-16 11:03:04 +0200

    fix offset filter range

--- Source/Common/Processor/Mapping/Filter/filters/OffsetFilter.cpp
+++ Source/Common/Processor/Mapping/Filter/filters/OffsetFilter.cpp
@@ -13,6 +13,7 @@
 OffsetFilter::OffsetFilter(var params) :
 	MappingFilter(getTypeString(),params)
 {
+	autoSetRange = false;
 	offset = filterParams.addFloatParameter("Offset", "The amount of offset to apply", 0);
 	offset->isCustomizableByUser = true;
 	forceOutParameterType = FloatParameter::getTypeStringStatic();
@@ -26,8 +27,14 @@ void OffsetFilter::processInternal()
 {
 	if (sourceParam->hasRange() && !sourceParam->value.isArray())
 	{
-		filteredParameter->setRange((float)sourceParam->minimumValue + offset->floatValue(), (float)sourceParam->maximumValue + offset->floatValue());
+		float tMin = (float)sourceParam->minimumValue + offset->floatValue();
+		float tMax = (float)sourceParam->maximumValue + offset->floatValue();
+		if (tMin != (float)filteredParameter->minimumValue || tMax != (float)filteredParameter->maximumValue)
+		{
+			filteredParameter->setRange(tMin, tMax);
+		}
 		filteredParameter->setNormalizedValue(sourceParam->getNormalizedValue());
+
 	} else
 	{
 		filteredParameter->setValue(sourceParam->floatValue() + offset->floatValue());
