b57403aa8574784745270fda2cdb60ef4fb5285f
Ben Kuper <bkuperberg@gmail.com>
2018-06-17 17:32:30 +0200

    1.4.7 functional crash dump

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -44,7 +44,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
-    const char* const  versionString  = "1.4.6";
-    const int          versionNumber  = 0x10406;
+    const char* const  versionString  = "1.4.7";
+    const int          versionNumber  = 0x10407;
 }
 #endif
