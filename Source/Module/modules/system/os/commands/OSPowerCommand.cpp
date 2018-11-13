af69fd4282d4bd6d3a090adcdfa7ca8f46d10a53
Ben Kuper <bkuperberg@gmail.com>
2018-11-13 18:51:45 +0100

    fix osx power commands

--- Source/Module/modules/system/os/commands/OSPowerCommand.cpp
+++ Source/Module/modules/system/os/commands/OSPowerCommand.cpp
@@ -31,7 +31,7 @@ void OSPowerCommand::trigger()
 #if JUCE_WINDOWS
 		std::system("shutdown -s -t 0");
 #elif JUCE_MAC
-		std::system("shutdown -s now");
+        std::system("osascript -e 'tell app \"System Events\" to shut down'"); 
 #elif JUCE_LINUX
 		int result = std::system("shutdown -h now");
 		if(result != 0) LOGWARNING("Problem with shutdown command");
@@ -44,7 +44,7 @@ void OSPowerCommand::trigger()
 #if JUCE_WINDOWS
 		std::system("shutdown -r -t 0");
 #elif JUCE_MAC
-		std::system("shutdown -r now");
+		std::system("osascript -e 'tell app \"System Events\" to restart'");
 #elif JUCE_LINUX
 		int result = std::system("shutdown -r now");
 		if(result != 0) LOGWARNING("Problem with reboot command");
