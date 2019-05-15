9838fa169a37b72e18628ebe20e7ed067f4154fa
Ben Kuper <bkuperberg@gmail.com>
2019-05-15 20:05:18 +0200

    add openFile, newFile, closeApp generic commands

--- Source/Module/modules/generic/ChataigneGenericModule.cpp
+++ Source/Module/modules/generic/ChataigneGenericModule.cpp
@@ -12,6 +12,7 @@
 #include "commands/GenericControllableCommand.h"
 #include "commands/ChataigneLogCommand.h"
 #include "commands/GenericScriptCommand.h"
+#include "commands/GenericAppCommand.h"
 
 ChataigneGenericModule::ChataigneGenericModule() :
 	Module("Generic")
@@ -20,6 +21,9 @@ ChataigneGenericModule::ChataigneGenericModule() :
 	defManager.add(CommandDefinition::createDef(this, "", "Trigger a Control", &GenericControllableCommand::create)->addParam("action", GenericControllableCommand::TRIGGER));
 	defManager.add(CommandDefinition::createDef(this, "", "Log a message", &ChataigneLogCommand::create, CommandContext::ACTION)->addParam("type", ChataigneLogCommand::MESSAGE));
 	defManager.add(CommandDefinition::createDef(this, "", "Log a value", &ChataigneLogCommand::create)->addParam("type", ChataigneLogCommand::VALUE));
+	defManager.add(CommandDefinition::createDef(this, "", "New Session", &GenericAppCommand::create, CommandContext::ACTION)->addParam("type", GenericAppCommand::NEW_SESSION));
+	defManager.add(CommandDefinition::createDef(this, "", "Open Session", &GenericAppCommand::create, CommandContext::ACTION)->addParam("type", GenericAppCommand::OPEN_SESSION));
+	defManager.add(CommandDefinition::createDef(this, "", "Close App", &GenericAppCommand::create, CommandContext::ACTION)->addParam("type", GenericAppCommand::CLOSE_APP));
 	defManager.add(CommandDefinition::createDef(this, "", "Script", &GenericScriptCommand::create));
 }
 
