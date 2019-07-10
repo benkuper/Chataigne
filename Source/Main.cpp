f756b93825526ac08beba297c5dbbb7e45d58411
Ben Kuper <bkuperberg@gmail.com>
2019-07-10 15:37:29 +0200

    update organicui help system for language support

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -16,12 +16,13 @@ ChataigneApplication::ChataigneApplication() :
 
 void ChataigneApplication::initialiseInternal(const String &)
 {
-	AppUpdater::getInstance()->setURLs(URL("http://benjamin.kuperberg.fr/chataigne/releases/update.json"), "http://benjamin.kuperberg.fr/chataigne/user/data/", "Chataigne");
-	HelpBox::getInstance()->helpURL = URL("http://benjamin.kuperberg.fr/chataigne/docs/help.json");
-	CrashDumpUploader::getInstance()->remoteURL = URL("http://benjamin.kuperberg.fr/chataigne/support/crash_report.php");
-
 	engine.reset(new ChataigneEngine());
 	mainComponent.reset(new MainContentComponent());
+
+	//Call after engine init
+	AppUpdater::getInstance()->setURLs(URL("http://benjamin.kuperberg.fr/chataigne/releases/update.json"), "http://benjamin.kuperberg.fr/chataigne/user/data/", "Chataigne");
+	HelpBox::getInstance()->helpURL = URL("http://benjamin.kuperberg.fr/chataigne/docs/");
+	CrashDumpUploader::getInstance()->remoteURL = URL("http://benjamin.kuperberg.fr/chataigne/support/crash_report.php");
 
 	ShapeShifterManager::getInstance()->setDefaultFileData(BinaryData::default_chalayout);
 	ShapeShifterManager::getInstance()->setLayoutInformations("chalayout", "Chataigne/layouts");
