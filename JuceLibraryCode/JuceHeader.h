041b0bf8a8da886e698bbef820360b7a6f0679e2
Ben Kuper <bkuperberg@gmail.com>
2019-09-16 11:59:26 +0200

    1.6.4b4 fix # not taken in names (MIDI notes save/load bug)

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -47,7 +47,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.6.4b3";
+    const char* const  versionString  = "1.6.4b4";
     const int          versionNumber  = 0x10604;
 }
 #endif
