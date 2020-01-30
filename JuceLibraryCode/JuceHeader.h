a1f9f762ac363edfdbd6cc92515915f192f2151e
Ben Kuper <bkuperberg@gmail.com>
2020-01-30 23:26:37 +0100

    added organicui and timeline as submodules, update travis and jucer

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.6.12b3";
+    const char* const  versionString  = "1.6.12b4";
     const int          versionNumber  = 0x1060c;
 }
 #endif
