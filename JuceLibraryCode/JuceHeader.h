e34305b90fc415e1c63678a3d9f509a66e4c2b43
Ben Kuper <bkuperberg@gmail.com>
2020-02-28 23:08:03 +0100

    1.6.13b3 organicUI update (dashboard+logger)

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.6.13b2";
+    const char* const  versionString  = "1.6.13b3";
     const int          versionNumber  = 0x1060d;
 }
 #endif
