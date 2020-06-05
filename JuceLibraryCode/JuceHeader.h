6e679950e481a2063b9d082a04517b68c7ef51ad
Ben Kuper <bkuperberg@gmail.com>
2020-06-05 11:46:51 +0200

    1.7.0b26  hotfix multiple audio layers bug

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.7.0b25";
+    const char* const  versionString  = "1.7.0b26";
     const int          versionNumber  = 0x10700;
 }
 #endif
