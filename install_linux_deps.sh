38f2ae7f4483fe4dbf380f7a4c5ef4c6c962d6dd
Ben Kuper <bkuperberg@gmail.com>
2020-06-10 14:47:41 -0700

    add easy dependency script for linux

---
+++ install_linux_deps.sh
@@ -0,0 +1,5 @@
+echo "Installing armhf cross compilation tools"
+sudo apt-get install -q g++
+
+echo "Installing extra lib dependencies"
+sudo apt-get install -q make libfreetype6-dev libx11-dev libxinerama-dev libxrandr-dev libxcursor-dev libxcomposite-dev mesa-common-dev libasound2-dev freeglut3-dev libcurl4-gnutls-dev libasound2-dev libjack-dev libbluetooth-dev libgtk-3-dev libwebkit2gtk-4.0-dev libsdl2-dev  libfuse2 libusb-1.0-0-dev libhidapi-dev
