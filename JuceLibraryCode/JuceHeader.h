fed02e691335ba00ab75fa566976a0386b707c61
Ben Kuper <bkuperberg@gmail.com>
2020-09-19 16:23:35 +0200

    1.7.5b9 organicUI and timeline update

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.7.5b8";
+    const char* const  versionString  = "1.7.5b9";
     const int          versionNumber  = 0x10705;
 }
 #endif
