1bd8599c7a183d03e051d2368621fd3af2cfcf91
Ben Kuper <bkuperberg@gmail.com>
2021-02-15 20:54:12 +0100

    fix compil

--- Source/Module/modules/osc/OSCModule.cpp
+++ Source/Module/modules/osc/OSCModule.cpp
@@ -678,9 +678,9 @@ OSCModule::OSCRouteParams::OSCRouteParams(Module * sourceModule, Controllable * 
 
 OSCOutput::OSCOutput() :
 	BaseItem("OSC Output"),
-	Thread("OSC output")
+	Thread("OSC output"),
 	forceDisabled(false),
-	senderIsConnected(false),
+	senderIsConnected(false)
 {
 	isSelectable = false;
 
