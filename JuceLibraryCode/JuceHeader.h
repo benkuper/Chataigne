78e7feca15f7135d3f6849e6891b5bb0f4a88ebb
Ben Kuper <bkuperberg@gmail.com>
2021-03-03 10:02:11 +0100

    1.8.0b16

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.8.0b15";
+    const char* const  versionString  = "1.8.0b16";
     const int          versionNumber  = 0x10800;
 }
 #endif
