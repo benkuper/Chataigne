56762516b4fe40ffe2a945e4320c325fa83c2bdd
Ben Kuper <bkuperberg@gmail.com>
2021-03-29 23:00:18 +0200

    formatting

--- Source/Common/Processor/Mapping/Filter/filters/number/SimpleRemapFilter.h
+++ Source/Common/Processor/Mapping/Filter/filters/number/SimpleRemapFilter.h
@@ -1,9 +1,9 @@
 /*
   ==============================================================================
 
-    SimpleRemapFilter.h
-    Created: 4 Feb 2017 5:39:07pm
-    Author:  Ben
+	SimpleRemapFilter.h
+	Created: 4 Feb 2017 5:39:07pm
+	Author:  Ben
 
   ==============================================================================
 */
@@ -18,20 +18,20 @@ public:
 	SimpleRemapFilter(var params, Multiplex* multiplex);
 	~SimpleRemapFilter();
 
-	BoolParameter * useCustomInputRange;
-	Point2DParameter * targetIn;
-	Point2DParameter * targetOut;
+	BoolParameter* useCustomInputRange;
+	Point2DParameter* targetIn;
+	Point2DParameter* targetOut;
 	BoolParameter* forceFloatOutput;
 
 	virtual bool setupSources(Array<Parameter*> sources, int multiplexIndex, bool rangeOnly) override;
-	Parameter * setupSingleParameterInternal(Parameter * source, int multiplexIndex, bool rangeOnly) override;
+	Parameter* setupSingleParameterInternal(Parameter* source, int multiplexIndex, bool rangeOnly) override;
 	ProcessResult processSingleParameterInternal(Parameter* source, Parameter* out, int multiplexIndex) override;
 
 	var getRemappedValueFor(Parameter* source, int multiplexIndex); //allow for child classes to invoke this 
 
 	void computeOutRanges();
 
-	void filterParamChanged(Parameter *) override;
+	void filterParamChanged(Parameter*) override;
 
 	String getTypeString() const override { return "Remap"; }
 
