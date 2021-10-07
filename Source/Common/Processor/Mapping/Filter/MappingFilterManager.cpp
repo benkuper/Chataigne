8ac516f0693aa3fdf4b9d4c7cd68267306849263
Ben Kuper <bkuperberg@gmail.com>
2021-10-07 08:53:08 +0200

    fix crash on input remove with different threads

--- Source/Common/Processor/Mapping/Filter/MappingFilterManager.cpp
+++ Source/Common/Processor/Mapping/Filter/MappingFilterManager.cpp
@@ -72,6 +72,7 @@ MappingFilter::ProcessResult MappingFilterManager::processFilters(Array<Paramete
 
 
 	jassert(inputs.size() == inputSources[multiplexIndex].size());
+	if (inputs.size() != inputSources[multiplexIndex].size()) return MappingFilter::STOP_HERE;
 
 	Array<Parameter *> fp = inputs;
 	MappingFilter::ProcessResult result = MappingFilter::UNCHANGED;
