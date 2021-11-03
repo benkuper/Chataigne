173b6f1d8d958506c47b78ab3a467a78bbd410ff
Ben Kuper <bkuperberg@gmail.com>
2021-11-03 12:02:44 +0100

    bmp in snapTimes

--- Source/TimeMachine/Sequence/layers/mapping/automation/AutomationMappingLayer.cpp
+++ Source/TimeMachine/Sequence/layers/mapping/automation/AutomationMappingLayer.cpp
@@ -1,28 +1,28 @@
 /*
   ==============================================================================
 
-    AutomationMappingLayer.cpp
-    Created: 25 Mar 2020 1:00:31pm
-    Author:  bkupe
+	AutomationMappingLayer.cpp
+	Created: 25 Mar 2020 1:00:31pm
+	Author:  bkupe
 
   ==============================================================================
 */
 
 
 AutomationMappingLayer::AutomationMappingLayer(const String& name, Sequence* s, var params) :
-    MappingLayer(name, s, params),
-    automation(nullptr)
+	MappingLayer(name, s, params),
+	automation(nullptr)
 {
-    recordSendMode = addEnumParameter("Record Send Mode", "Choose what to do when recording");
-    recordSendMode->addOption("Do not send", DONOTSEND)->addOption("Send original value", SEND_ORIGINAL)->addOption("Send new value", SEND_NEW);
+	recordSendMode = addEnumParameter("Record Send Mode", "Choose what to do when recording");
+	recordSendMode->addOption("Do not send", DONOTSEND)->addOption("Send original value", SEND_ORIGINAL)->addOption("Send new value", SEND_NEW);
 
-    recorder.input->customGetTargetFunc = &ModuleManager::showAllValuesAndGetControllable;
-    recorder.input->customGetControllableLabelFunc = &Module::getTargetLabelForValueControllable;
-    recorder.input->customCheckAssignOnNextChangeFunc = &ModuleManager::checkControllableIsAValue;
-    recorder.editorIsCollapsed = true;
-    addChildControllableContainer(&recorder,false, 0);
+	recorder.input->customGetTargetFunc = &ModuleManager::showAllValuesAndGetControllable;
+	recorder.input->customGetControllableLabelFunc = &Module::getTargetLabelForValueControllable;
+	recorder.input->customCheckAssignOnNextChangeFunc = &ModuleManager::checkControllableIsAValue;
+	recorder.editorIsCollapsed = true;
+	addChildControllableContainer(&recorder, false, 0);
 
-    uiHeight->setValue(120);
+	uiHeight->setValue(120);
 }
 
 AutomationMappingLayer::~AutomationMappingLayer()
@@ -31,154 +31,148 @@ AutomationMappingLayer::~AutomationMappingLayer()
 
 void AutomationMappingLayer::setupAutomation(Automation* a)
 {
-    jassert(automation == nullptr);
-    automation = a;
-    //automation->hideInEditor = true;
-    automation->setLength(sequence->totalTime->floatValue(), true);
-    automation->length->setControllableFeedbackOnly(true); //force not saving and not changing from user
-    automation->length->isSavable = false;
-    if(ChataigneSequenceManager::getInstance()->snapKeysToFrames->boolValue()) automation->setUnitSteps(sequence->fps->intValue());
+	jassert(automation == nullptr);
+	automation = a;
+	//automation->hideInEditor = true;
+	automation->setLength(sequence->totalTime->floatValue(), true);
+	automation->length->setControllableFeedbackOnly(true); //force not saving and not changing from user
+	automation->length->isSavable = false;
+	if (ChataigneSequenceManager::getInstance()->snapKeysToFrames->boolValue()) automation->setUnitSteps(sequence->fps->intValue());
 }
 
 void AutomationMappingLayer::updateMappingInputValueInternal()
 {
-    if (!recorder.isRecording->boolValue())
-    {
-        MappingLayer::updateMappingInputValueInternal();
-    }
-    else
-    {
-        RecordSendMode m = recordSendMode->getValueDataAsEnum<RecordSendMode>();
-        if (m == SEND_ORIGINAL)
-        {
-            MappingLayer::updateMappingInputValueInternal();
-        }
-        else if (m == SEND_NEW)
-        {
-            if (recorder.keys.size() > 0) mappingInput->setValue(recorder.keys[recorder.keys.size() - 1].value);
-        }
-    }
+	if (!recorder.isRecording->boolValue())
+	{
+		MappingLayer::updateMappingInputValueInternal();
+	}
+	else
+	{
+		RecordSendMode m = recordSendMode->getValueDataAsEnum<RecordSendMode>();
+		if (m == SEND_ORIGINAL)
+		{
+			MappingLayer::updateMappingInputValueInternal();
+		}
+		else if (m == SEND_NEW)
+		{
+			if (recorder.keys.size() > 0) mappingInput->setValue(recorder.keys[recorder.keys.size() - 1].value);
+		}
+	}
 
 }
 
 void AutomationMappingLayer::selectAll(bool addToSelection)
 {
-    deselectThis(automation->items.size() == 0);
-    automation->askForSelectAllItems(addToSelection);
+	deselectThis(automation->items.size() == 0);
+	automation->askForSelectAllItems(addToSelection);
 }
 
 Array<Inspectable*> AutomationMappingLayer::selectAllItemsBetweenInternal(float start, float end)
 {
-    Array<Inspectable*> result;
-    Array<AutomationKey*> keys = automation->getKeysBetweenPositions(start, end);
-    result.addArray(keys);
-    return result;
+	Array<Inspectable*> result;
+	Array<AutomationKey*> keys = automation->getKeysBetweenPositions(start, end);
+	result.addArray(keys);
+	return result;
 }
 
 Array<UndoableAction*> AutomationMappingLayer::getRemoveAllItemsBetweenInternal(float start, float end)
 {
-    return automation->getRemoveItemsUndoableAction(automation->getKeysBetweenPositions(start, end));
+	return automation->getRemoveItemsUndoableAction(automation->getKeysBetweenPositions(start, end));
 }
 
 Array<UndoableAction*> AutomationMappingLayer::getInsertTimespanInternal(float start, float length)
 {
-    return automation->getMoveKeysBy(start, length);
+	return automation->getMoveKeysBy(start, length);
 }
 
 Array<UndoableAction*> AutomationMappingLayer::getRemoveTimespanInternal(float start, float end)
 {
-    return automation->getRemoveTimespan(start, end);
+	return automation->getRemoveTimespan(start, end);
 }
 
 void AutomationMappingLayer::getSnapTimes(Array<float>* arrayToFill)
 {
-    if (automation == nullptr) return;
-    for (auto& i : automation->items)
-    {
-        arrayToFill->addIfNotAlreadyThere(i->position->floatValue());
-    }
+	if (automation == nullptr) return;
+	for (auto& i : automation->items)
+	{
+		arrayToFill->addIfNotAlreadyThere(i->position->floatValue());
+	}
 }
 
-void AutomationMappingLayer::getSequenceSnapTimesForAutomation(Array<float>* arrayToFill, AutomationKey * k)
+void AutomationMappingLayer::getSequenceSnapTimesForAutomation(Array<float>* arrayToFill, AutomationKey* k)
 {
-    sequence->getSnapTimes(arrayToFill);
-    int index = automation->items.indexOf(k);
-    AutomationKey* k1 = index > 0 ? automation->items[index - 1] : nullptr;
-    AutomationKey* k2 = index < automation->items.size() - 1 ? automation->items[index + 1] : nullptr;
+	int index = automation->items.indexOf(k);
 
-    float t1 = k1 != nullptr ? k1->position->floatValue() : 0;
-    float t2 = k2 != nullptr ? k2->position->floatValue() : sequence->totalTime->floatValue();
+	AutomationKey* k1 = index > 0 ? automation->items[index - 1] : nullptr;
+	AutomationKey* k2 = index < automation->items.size() - 1 ? automation->items[index + 1] : nullptr;
 
-    Array<float> keysToRemove;
-    for (auto& t : *arrayToFill)
-    {
-        if (t <= t1 || t >= t2) keysToRemove.add(t);
-    }
+	float t1 = k1 != nullptr ? k1->position->floatValue() : 0;
+	float t2 = k2 != nullptr ? k2->position->floatValue() : sequence->totalTime->floatValue();
 
-    for (auto& t : keysToRemove) arrayToFill->removeAllInstancesOf(t);
+	sequence->getSnapTimes(arrayToFill, t1, t2);
 }
 
 
 void AutomationMappingLayer::sequenceCurrentTimeChangedInternal(Sequence* s, float prevTime, bool seeking)
 {
-    if (automation == nullptr) return;
+	if (automation == nullptr) return;
 
-    float curTime = sequence->currentTime->floatValue();
-    automation->position->setValue(curTime);
+	float curTime = sequence->currentTime->floatValue();
+	automation->position->setValue(curTime);
 
-    if (sequence->isPlaying->boolValue())
-    {
-        if (recorder.isRecording->boolValue())
-        {
-            if (sequence->currentTime->floatValue() < prevTime)  recorder.removeKeysAfter(curTime);
-            recorder.addKeyAt(curTime);
-        }
-    }
+	if (sequence->isPlaying->boolValue())
+	{
+		if (recorder.isRecording->boolValue())
+		{
+			if (sequence->currentTime->floatValue() < prevTime)  recorder.removeKeysAfter(curTime);
+			recorder.addKeyAt(curTime);
+		}
+	}
 }
 
 void AutomationMappingLayer::sequenceTotalTimeChanged(Sequence* s)
 {
-    automation->setLength(s->totalTime->floatValue());
+	automation->setLength(s->totalTime->floatValue());
 }
 
 void AutomationMappingLayer::sequencePlayStateChangedInternal(Sequence* s)
 {
-    if (sequence->isPlaying->boolValue())
-    {
-        if (recorder.shouldRecord()) recorder.startRecording();
-    }
-    else if (recorder.isRecording->boolValue())
-    {
-        stopRecorderAndAddKeys();
-    }
+	if (sequence->isPlaying->boolValue())
+	{
+		if (recorder.shouldRecord()) recorder.startRecording();
+	}
+	else if (recorder.isRecording->boolValue())
+	{
+		stopRecorderAndAddKeys();
+	}
 }
 
 void AutomationMappingLayer::sequenceLooped(Sequence* s)
 {
-    if(recorder.isRecording->boolValue()) stopRecorderAndAddKeys();
+	if (recorder.isRecording->boolValue()) stopRecorderAndAddKeys();
 }
 
 bool AutomationMappingLayer::paste()
 {
-    var data = JSON::fromString(SystemClipboard::getTextFromClipboard());
-    String type = data.getProperty("itemType", "");
-    if (type == automation->itemDataType)
-    {
-        automation->askForPaste();
-        return true;
-    }
+	var data = JSON::fromString(SystemClipboard::getTextFromClipboard());
+	String type = data.getProperty("itemType", "");
+	if (type == automation->itemDataType)
+	{
+		automation->askForPaste();
+		return true;
+	}
 
-    return SequenceLayer::paste();
+	return SequenceLayer::paste();
 }
 
 
 SequenceLayerPanel* AutomationMappingLayer::getPanel()
 {
-    return new AutomationMappingLayerPanel(this);
+	return new AutomationMappingLayerPanel(this);
 }
 
 SequenceLayerTimeline* AutomationMappingLayer::getTimelineUI()
 {
-    return new AutomationMappingLayerTimeline(this);
+	return new AutomationMappingLayerTimeline(this);
 }
 
