00caf1a21057d189c58c470d6db86962ea349c36
Ben Kuper <bkuperberg@gmail.com>
2022-02-01 11:34:19 +0100

    project update

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -43,7 +43,7 @@
 
 #define JUCE_USE_DARK_SPLASH_SCREEN 1
 
-#define JUCE_PROJUCER_VERSION 0x60104
+#define JUCE_PROJUCER_VERSION 0x60105
 
 //==============================================================================
 #define JUCE_MODULE_AVAILABLE_juce_audio_basics          1
