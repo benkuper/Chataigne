5bd2f42c6c3f17a9d297887377625f279ca45086
Ben Kuper <bkuperberg@gmail.com>
2022-09-08 14:24:12 +0200

    b2 added SHA256 in script util

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.9.8b1";
+    const char* const  versionString  = "1.9.8b2";
     const int          versionNumber  = 0x10908;
 }
 #endif
