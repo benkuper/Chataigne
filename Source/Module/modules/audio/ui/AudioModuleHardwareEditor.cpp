728ff98acffb8bad26f1b42e548d9e1cd2856f66
Ben Kuper <bkuperberg@gmail.com>
2022-11-01 17:07:51 +0100

    limit to 64 channels

--- Source/Module/modules/audio/ui/AudioModuleHardwareEditor.cpp
+++ Source/Module/modules/audio/ui/AudioModuleHardwareEditor.cpp
@@ -1,22 +1,24 @@
 /*
   ==============================================================================
 
-    AudioModuleHardwareEditor.cpp
-    Created: 4 Jan 2018 1:45:32pm
-    Author:  Ben
+	AudioModuleHardwareEditor.cpp
+	Created: 4 Jan 2018 1:45:32pm
+	Author:  Ben
 
   ==============================================================================
 */
 
-AudioModuleHardwareEditor::AudioModuleHardwareEditor(AudioModuleHardwareSettings * hs, bool isRoot) :
-	GenericControllableContainerEditor(hs,isRoot),
+#include "Module/ModuleIncludes.h"
+
+AudioModuleHardwareEditor::AudioModuleHardwareEditor(AudioModuleHardwareSettings* hs, bool isRoot) :
+	GenericControllableContainerEditor(hs, isRoot),
 	hs(hs),
-	selector(*hs->am, 0, 16, 0, 16, false, false, false, false)
+	selector(*hs->am, 0, 64, 0, 64, false, false, false, false)
 {
 	selector.setSize(100, 300);
 	selector.setVisible(!container->editorIsCollapsed);
 	addAndMakeVisible(selector);
-	if(selector.isVisible()) setSize(100,selector.getBottom() + headerHeight + headerGap);
+	if (selector.isVisible()) setSize(100, selector.getBottom() + headerHeight + headerGap);
 }
 
 AudioModuleHardwareEditor::~AudioModuleHardwareEditor()
