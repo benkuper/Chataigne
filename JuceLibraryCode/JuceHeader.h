95899db35ba3cf88a91f87c8ecda20dd349a4a80
Ben Kuper <bkuperberg@gmail.com>
2022-11-27 10:56:26 +0100

    1.9.12b1 organic and timeline fixes

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -56,7 +56,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.9.11";
-    const int          versionNumber  = 0x1090b;
+    const char* const  versionString  = "1.9.12b1";
+    const int          versionNumber  = 0x1090c;
 }
 #endif
