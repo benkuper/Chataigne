646628a7cfe2bf9d7657559d24efafcab8dce343
Ben Kuper <bkuperberg@gmail.com>
2022-12-05 14:05:31 +0100

    version in AboutWindow

--- Source/UI/AboutWindow.cpp
+++ Source/UI/AboutWindow.cpp
@@ -1,12 +1,14 @@
 /*
   ==============================================================================
 
-    AboutWindow.cpp
-    Created: 4 Jan 2018 7:25:58pm
-    Author:  Ben
+	AboutWindow.cpp
+	Created: 4 Jan 2018 7:25:58pm
+	Author:  Ben
 
   ==============================================================================
 */
+
+#include "MainIncludes.h"
 
 AboutWindow::AboutWindow() :
 	Component("About")
@@ -19,8 +21,12 @@ AboutWindow::~AboutWindow()
 {
 }
 
-void AboutWindow::paint(Graphics &g) 
+void AboutWindow::paint(Graphics& g)
 {
 	g.fillAll(BG_COLOR.darker());
 	g.drawImage(aboutImage, getLocalBounds().toFloat());
+
+	g.setColour(TEXT_COLOR);
+	g.setFont(12);
+	g.drawText(getApp().getApplicationName() + " " + getApp().getApplicationVersion(), getLocalBounds().removeFromBottom(30).removeFromRight(200).reduced(5).toFloat(), Justification::right);
 }
