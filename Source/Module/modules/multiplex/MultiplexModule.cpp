8b8fc3dd8032123448a927c6592bbb24a79414f6
Ben Kuper <bkuperberg@gmail.com>
2023-03-25 11:21:18 +0100

    Multiplex trigger list command

--- Source/Module/modules/multiplex/MultiplexModule.cpp
+++ Source/Module/modules/multiplex/MultiplexModule.cpp
@@ -8,11 +8,16 @@
   ==============================================================================
 */
 
+#include "Module/ModuleIncludes.h"
+
 MultiplexModule::MultiplexModule() :
 	Module("Multiplex")
 {
 	defManager->add(CommandDefinition::createDef(this, "", "Set List Value", &MultiplexCommand::create)->addParam("action", GenericControllableCommand::SET_VALUE));
+
 	defManager->add(CommandDefinition::createDef(this, "", "Go to List Value", &MultiplexCommand::create)->addParam("action", GenericControllableCommand::GO_TO_VALUE));
+
+	defManager->add(CommandDefinition::createDef(this, "", "Trigger List", &MultiplexCommand::create)->addParam("action", GenericControllableCommand::TRIGGER));
 }
 
 MultiplexModule::~MultiplexModule()
