5c5be7980c2e8eb4db544a55af82e38a3b929ec3
Ben Kuper <bkuperberg@gmail.com>
2023-04-09 17:45:23 +0200

    trying again os module

--- Source/Module/modules/system/os/OSModule.cpp
+++ Source/Module/modules/system/os/OSModule.cpp
@@ -438,7 +438,7 @@ void OSModule::PingThread::run()
 #if JUCE_WINDOWS
 			bool success = result.contains("Received = 1");
 #else
-			bool success = !result.matchesWildcard("100*% packet loss", true);
+			bool success = !result.contains(" 0.0% packet loss", true);
 #endif
 
 			if (osModule->logOutgoingData->boolValue())
