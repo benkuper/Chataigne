12bca33a8fd648f087b3ee6a852b1040c9a85e8c
Ben Kuper <bkuperberg@gmail.com>
2024-08-27 18:22:41 -0400

    1.9.20b6 crash report fix

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -58,7 +58,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.9.20b5";
+    const char* const  versionString  = "1.9.20b6";
     const int          versionNumber  = 0x10914;
 }
 #endif
