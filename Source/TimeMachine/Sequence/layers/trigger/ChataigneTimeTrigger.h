c4e0276eda41bf7bc9cd39d94c383b8794ebb22a
Emerick Herve <emerick@emerickh.fr>
2025-03-16 19:17:06 +0100

    Conditionnal triggers

--- Source/TimeMachine/Sequence/layers/trigger/ChataigneTimeTrigger.h
+++ Source/TimeMachine/Sequence/layers/trigger/ChataigneTimeTrigger.h
@@ -13,19 +13,50 @@
 class ConsequenceManager;
 
 class ChataigneTimeTrigger :
+	public ConditionManagerListener,
 	public TimeTrigger
 {
 public:
 	ChataigneTimeTrigger(StringRef name = "Trigger");
 	virtual ~ChataigneTimeTrigger();
 
+	enum evaluateSetting
+    {
+        onlyOnce       = 0,
+        onlyEnterExit  = 1,
+        always         = 2,
+    };
+
+	enum possibleActions
+    {
+        action_nothing       = 0,
+        action_deactivate    = 1,
+        action_triggerTrue   = 2,
+        action_triggerFalse  = 3,
+		action_triggerBoth   = 4
+    };
+
+	EnumParameter * triggerCondition;
+	EnumParameter * onValidExit;
+	EnumParameter * onInvalidExit;
+	EnumParameter * onRewind;
+
+	bool alreadyTriggered;
+
 	std::unique_ptr<ConsequenceManager> csm;
 	std::unique_ptr<ConsequenceManager> untcsm;
 
+	std::unique_ptr<ConditionManager> cdm;
+
+	void updateTriggerParams();
+
 	virtual void onContainerParameterChangedInternal(Parameter* p) override;
+
+	virtual void conditionManagerValidationChanged(ConditionManager*, int multiplexIndex, bool dispatchOnChangeOnly) override;
 
 	virtual void triggerInternal() override;
 	virtual void unTriggerInternal() override;
+	virtual void exitedInternal(bool force) override;
 
 	virtual var getJSONData(bool includeNonOverriden = false) override;
 	virtual void loadJSONDataInternal(var data) override;
