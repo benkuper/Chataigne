cb9d2e887c7d98dd1e029f14c449a02f78b5e74e
Ben Kuper <bkuperberg@gmail.com>
2025-03-28 11:28:47 +0100

    b12 fix audio enveloppe right click

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -58,7 +58,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Chataigne";
     const char* const  companyName    = "Ben Kuper";
-    const char* const  versionString  = "1.9.25b11";
+    const char* const  versionString  = "1.9.25b12";
     const int          versionNumber  = 0x10919;
 }
 #endif
